0f715479a6e9353c381f7601e1d23586c03dc44a
moskalevNick <moskalevnikolay1@gmail.com>
2023-07-17 23:45:00 +0300

    fix header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,104 +1,180 @@
-import { NavLink } from 'react-router-dom';
-import logonan from '../../resources/logo/logonan.svg'
-import logofull from '../../resources/logo/logofull.svg'
-import OKB from '../../resources/logo/OKB.svg'
-import peleng from '../../resources/subHeader/peleng.svg'
-import natrix from '../../resources/subHeader/Natrix.svg'
-import et from '../../resources/subHeader/et.svg'
-import naftuk from '../../resources/subHeader/naftuk.svg'
-import mnpz from '../../resources/subHeader/mnpz.svg'
-import belaES from '../../resources/subHeader/BelaES.svg'
-import belarusianneft from '../../resources/subHeader/Belarusianneft.svg'
-import integral from '../../resources/subHeader/integral.svg'
-import mogilevkhimvolokno from '../../resources/subHeader/mogilevkhimvolokno.svg'
+import { NavLink, useLocation } from 'react-router-dom';
+import logonan from '../../resources/logo/logonan.svg';
+import logofull from '../../resources/logo/logofull.svg';
+import OKB from '../../resources/logo/OKB.svg';
+import peleng from '../../resources/subHeader/peleng.svg';
+import natrix from '../../resources/subHeader/Natrix.svg';
+import et from '../../resources/subHeader/et.svg';
+import naftuk from '../../resources/subHeader/naftuk.svg';
+import mnpz from '../../resources/subHeader/mnpz.svg';
+import belaES from '../../resources/subHeader/BelaES.svg';
+import belarusianneft from '../../resources/subHeader/Belarusianneft.svg';
+import integral from '../../resources/subHeader/integral.svg';
+import mogilevkhimvolokno from '../../resources/subHeader/mogilevkhimvolokno.svg';
 
 import styles from './Header.module.css';
+import classNames from 'classnames';
+import { useEffect, useState } from 'react';
 
 export const Header = () => {
-  const activeStyle = {
-    fontWeight: '400',
-    color: '#1487F2',
-    transition: 'all .2s',
-  };
+  const { pathname } = useLocation();
+
+  const [activeLink, setActiveLink] = useState(0);
+  const [isReverse, setIsRevers] = useState(true);
+
+  useEffect(() => {
+    let mainPath = pathname.substring(1);
+
+    if (mainPath.indexOf('/')) {
+      mainPath = mainPath.split('/')[0];
+    }
+
+    if (mainPath)
+      switch (mainPath) {
+        case 'portfolio':
+          setActiveLink(1);
+          break;
+        case 'laboratory':
+          setActiveLink(2);
+          break;
+        case 'gems':
+          setActiveLink(3);
+          break;
+        case 'vacancies':
+          setActiveLink(4);
+          break;
+        case 'contacts':
+          setActiveLink(5);
+          break;
+        default:
+          break;
+      }
+  }, [pathname]);
 
   return (
-      <div className={styles.wrapper}>
-        <NavLink 
-          to='/'
+    <div className={styles.wrapper}>
+      <NavLink
+        to='/'
+        end
+        className={styles.wrapperLogo}
+        onClick={() => {
+          setActiveLink(0);
+        }}
+      >
+        <img src={logonan} alt='logonan' />
+        <div className={styles.wrapperFull}>
+          <img src={logofull} alt='logofull' />
+          <div className={styles.wrapperOKB}>
+            <img src={OKB} alt='OKB' />
+            <span>Академическое</span>
+          </div>
+        </div>
+      </NavLink>
+      <div className={styles.wrapperSection}>
+        <NavLink
+          to='/portfolio'
           end
-          className={styles.wrapperLogo}
+          className={classNames(
+            styles.section,
+            activeLink === 1 && styles.activeSection,
+            isReverse && activeLink === 1 && styles.reverseAnimation
+          )}
+          onClick={() => {
+            setActiveLink(1);
+            !isReverse && setIsRevers(true);
+          }}
         >
-          <img src={logonan} alt="logonan" />
-          <div className={styles.wrapperFull}>
-            <img src={logofull} alt="logofull"/>
-            <div className={styles.wrapperOKB}>
-              <img src={OKB} alt="OKB" />
-              <span>Академическое</span>
-            </div>
-          </div>
+          ПОРТФОЛИО
         </NavLink>
-        <div className={styles.wrapperSection}>
-          <NavLink
-            to='/portfolio'
-            end
-            className={styles.section}
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            ПОРТФОЛИО
-          </NavLink>
-          <NavLink
-            to='/laboratory'
-            className={styles.section}
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            ЛАБОРАТОРИЯ
-          </NavLink>
-          <NavLink
-            to='/gems'
-            className={styles.section}
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            АЛМАЗЫ
-          </NavLink>
-          <NavLink
-            to='/vacancies'
-            className={styles.section}
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            ВАКАНСИИ
-          </NavLink>
-          <NavLink
-            to='/contacts'
-            className={styles.section}
-            style={({ isActive }) => (isActive ? activeStyle : undefined)}
-          >
-            КОНТАКТЫ
-          </NavLink>
+        <NavLink
+          to='/laboratory'
+          className={classNames(
+            styles.section,
+            activeLink === 2 && styles.activeSection,
+            isReverse && activeLink === 2 && styles.reverseAnimation
+          )}
+          onClick={() => {
+            if (activeLink > 2) {
+              setIsRevers(true);
+            } else setIsRevers(false);
+            setActiveLink(2);
+          }}
+        >
+          ЛАБОРАТОРИЯ
+        </NavLink>
+        <NavLink
+          to='/gems'
+          className={classNames(
+            styles.section,
+            activeLink === 3 && styles.activeSection,
+            isReverse && activeLink === 3 && styles.reverseAnimation
+          )}
+          onClick={() => {
+            if (activeLink > 3) {
+              setIsRevers(true);
+            } else setIsRevers(false);
+            setActiveLink(3);
+          }}
+        >
+          АЛМАЗЫ
+        </NavLink>
+        <NavLink
+          to='/vacancies'
+          className={classNames(
+            styles.section,
+            activeLink === 4 && styles.activeSection,
+            isReverse && activeLink === 4 && styles.reverseAnimation
+          )}
+          onClick={() => {
+            if (activeLink > 4) {
+              setIsRevers(true);
+            } else setIsRevers(false);
+            setActiveLink(4);
+          }}
+        >
+          ВАКАНСИИ
+        </NavLink>
+        <NavLink
+          to='/contacts'
+          className={classNames(
+            styles.section,
+            activeLink === 5 && styles.activeSection,
+            isReverse && activeLink === 5 && styles.reverseAnimation
+          )}
+          onClick={() => {
+            if (activeLink > 5) {
+              setIsRevers(true);
+            } else setIsRevers(false);
+            setActiveLink(5);
+          }}
+        >
+          КОНТАКТЫ
+        </NavLink>
+      </div>
+      <div className={styles.subHeader}>
+        <div className={styles.wrapperFirstHalf}>
+          <img src={peleng} alt='peleng' />
+          <img src={natrix} alt='natrix' />
+          <img src={et} alt='et' />
+          <img src={naftuk} alt='naftuk' />
+          <img src={mnpz} alt='mnpz' />
+          <img src={belaES} alt='belaES' />
+          <img src={belarusianneft} alt='belarusianneft' />
+          <img src={integral} alt='integral' />
+          <img src={mogilevkhimvolokno} alt='mogilevkhimvolokno' />
         </div>
-        <div className={styles.subHeader}>
-          <div className={styles.wrapperFirstHalf}>
-            <img src={peleng} alt="peleng" />
-            <img src={natrix} alt="natrix" />
-            <img src={et} alt="et" />
-            <img src={naftuk} alt="naftuk" />
-            <img src={mnpz} alt="mnpz" />
-            <img src={belaES} alt="belaES" />
-            <img src={belarusianneft} alt="belarusianneft" />
-            <img src={integral} alt="integral" />
-            <img src={mogilevkhimvolokno} alt="mogilevkhimvolokno" />
-          </div>
-          <div className={styles.wrapperSecondHalf}>
-            <img src={peleng} alt="peleng" />
-            <img src={natrix} alt="natrix" />
-            <img src={et} alt="et" />
-            <img src={naftuk} alt="naftuk" />
-            <img src={mnpz} alt="mnpz" />
-            <img src={belaES} alt="belaES" />
-            <img src={belarusianneft} alt="belarusianneft" />
-            <img src={integral} alt="integral" />
-            <img src={mogilevkhimvolokno} alt="mogilevkhimvolokno" />
-          </div>
+        <div className={styles.wrapperSecondHalf}>
+          <img src={peleng} alt='peleng' />
+          <img src={natrix} alt='natrix' />
+          <img src={et} alt='et' />
+          <img src={naftuk} alt='naftuk' />
+          <img src={mnpz} alt='mnpz' />
+          <img src={belaES} alt='belaES' />
+          <img src={belarusianneft} alt='belarusianneft' />
+          <img src={integral} alt='integral' />
+          <img src={mogilevkhimvolokno} alt='mogilevkhimvolokno' />
         </div>
+      </div>
     </div>
   );
 };
