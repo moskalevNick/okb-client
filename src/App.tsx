39b9c63f7c13e631d6a1c6b83ce11b66a36026dc
Timofey <yakubovich96@mail.ru>
2023-07-18 18:08:26 +0300

    new_WrapHeader1

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { HomePage } from './modules/HomePage/HomePage';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import { PortfolioPage } from './modules/PortfolioPage/PortfolioPage';
 import { LaboratoryPage } from './modules/LaboratoryPage/LaboratoryPage';
-import { GemsPage } from './modules/GemsPage/GemsPage';
+// import { GemsPage } from './modules/GemsPage/GemsPage';
 import { VacanciesPage } from './modules/VacanciesPage/VacanciesPage';
 import { ContactsPage } from './modules/ContactsPage/ContactsPage';
 import { Layout } from './modules/Layout/Layout';
@@ -16,7 +16,7 @@ function App() {
           <Route path='/' element={<HomePage />} />
           <Route path='/portfolio' element={<PortfolioPage />} />
           <Route path='/laboratory' element={<LaboratoryPage />} />
-          <Route path='/gems' element={<GemsPage />} />
+          {/* <Route path='/gems' element={<GemsPage />} /> */}
           <Route path='/vacancies' element={<VacanciesPage />} />
           <Route path='/contacts' element={<ContactsPage />} />
         </Route>
