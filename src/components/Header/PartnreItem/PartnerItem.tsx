a6cdd94e7bca68928c4055f8605f9ad1c81de04a
moskalevNick <moskalevnikolay1@gmail.com>
2023-08-10 17:37:04 +0300

    fix logo

--- src/components/Header/PartnreItem/PartnerItem.tsx
+++ src/components/Header/PartnreItem/PartnerItem.tsx
@@ -1,24 +1,27 @@
 import React from 'react';
 import { partnerType } from '../../../types/partner';
 
-import styles from './PartnerItem.module.css'
+import styles from './PartnerItem.module.css';
 
 type PartnerItemType = {
-    partner: partnerType;
+  partner: partnerType;
 };
-  
 
-const PartnerItem: React.FC<PartnerItemType> = ({partner}) => {
-
-    return (
-        <>
-                <div className={styles.partnreItem}>
-                    <img src={partner.logo.publicUrl} alt={`partnerImage_${partner.id}`} />
-                    <div>{partner.text}</div>
-                </div>
-        </>
-        
-    );
+const PartnerItem: React.FC<PartnerItemType> = ({ partner }) => {
+  let logo;
+  const fectlogo = async () => {
+    logo = await fetch(partner.logo.publicUrl, { method: 'get' });
+    return;
+  };
+  fectlogo();
+  return (
+    <>
+      <div className={styles.partnreItem}>
+        {logo}
+        <div>{partner.text}</div>
+      </div>
+    </>
+  );
 };
 
 export default PartnerItem;
